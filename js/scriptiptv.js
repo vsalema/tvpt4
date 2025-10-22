114802b83edc7e0547521222b9e8a2d3bdc00e4a
salema <114165491+vsalema@users.noreply.github.com>
2025-10-22 09:18:42 +0200

    Update scriptiptv.js

--- js/scriptiptv.js
+++ js/scriptiptv.js
@@ -465,7 +465,6 @@ function closePanel(){
       const file = input.files?.[0];
       if (!file) return;
       const txt = await file.text();
-      updateM3UTotal(text);
       channels = await parseM3U(txt);
       renderCategories(channels, $('#categorySelect'), $('#categorySelect2'));
       renderChannels(channels, $('#channelList'), $('#channelList2'));
@@ -1259,7 +1258,6 @@ const cat2 = document.getElementById('categorySelect2');
     if (!file) return;
     try {
       const text = await file.text();
-      updateM3UTotal(text);
       const json = JSON.parse(text);
 
       // validation légère
